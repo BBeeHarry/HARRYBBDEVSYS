45bc354c829304e948122bfc3595b5b12e76d9bb
Ratchanok <ratchanok24bee@hotmail.com>
2019-03-17 17:09:29 +0700

    get upload adjust and reund controller,view model,service

---
+++ BBDEVSYS/Controllers/Adjustrefund/AdjustRefundUploadController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBDEVSYS.Controllers.Adjustrefund
+{
+    public class AdjustRefundUploadController : Controller
+    {
+        // GET: AdjustRefundUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AdjustRefundUpload/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdjustRefundUpload/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdjustRefundUpload/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdjustRefundUpload/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdjustRefundUpload/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdjustRefundUpload/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdjustRefundUpload/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BBDEVSYS/ViewModels/Adjustrefund/AdjustrefundUploadViewModel.cs
@@ -0,0 +1,61 @@
+﻿using BBDEVSYS.Content.text;
+using BBDEVSYS.Services.Shared;
+using BBDEVSYS.ViewModels.Shared;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBDEVSYS.ViewModels.Adjustrefund
+{
+    [FluentValidation.Attributes.Validator(typeof(AdjustrefundUploadViewModelValidator))]
+    public class AdjustrefundUploadViewModel : IViewModel
+    {
+        public AdjustrefundUploadViewModel()
+        {
+           
+            AttachmentList = new List<AttachmentViewModel>();
+
+          
+        }
+        //Authorization
+        public const string RoleForManageData = "Role_MA_Invoice_Upload";
+        public const string RoleForDisplayData = "Role_DS_Invoice_Upload";
+
+        public const string ProcessCode = "InvoiceUploadProcess";
+        
+        [Display(Name = "INV_MONTH", ResourceType = typeof(ResourceText))]
+        public Nullable<int> INV_MONTH { get; set; }
+      
+        public string UPLOAD_BY { get; set; }
+        public Nullable<System.DateTime> UPLOAD_DATE { get; set; }
+
+        public string UploadStatus { get; set; }
+
+        public string Message { get; set; }
+
+        //Text
+        public string InvoiceTypeName { get; set; }
+        public string UploadByName { get; set; }
+
+      
+        //Screen 
+        public virtual bool UserSuperAdmin { get; set; }
+        public virtual string FileName { get; set; }
+        public List<AttachmentViewModel> AttachmentList { get;  set; }
+    }
+
+    public class AdjustrefundUploadViewModelValidator : AbstractValidator<AdjustrefundUploadViewModel>
+    {
+        public AdjustrefundUploadViewModelValidator()
+        {
+
+            ValidatorOptions.ResourceProviderType = typeof(ValidatorMessage);
+            //InvoiceUploadViewModel model = new InvoiceUploadViewModel();
+            //UtilityService.SetRuleForStringLength<InvoiceUploadViewModel, "">(this);
+        }
+    }
+}
